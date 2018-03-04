99b25aa1e7c2c5fdcba11fbe423bc9faff8f53cc
HanGuo97 <alex.guohan1106@outlook.com>
2018-03-04 14:49:29 -0500

    minor changes

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 import os
+import platform
 from setuptools import setup, find_packages
 
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def tensorflow_dependency():
+    if platform.system() == "Linux":
+        return "tensorflow-gpu>=1.5.0"
+    else:
+        return "tensorflow>=1.5.0"
 
 
 setup(
@@ -13,6 +21,6 @@ setup(
     description="Tensorflow helper library",
     long_description=read("README.md"),
     install_requires=[
-        "numpy", "tensorflow-gpu>=1.5.0",
+        "numpy", tensorflow_dependency(),
     ],
 )
