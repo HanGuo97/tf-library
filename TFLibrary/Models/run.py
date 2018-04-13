5d97021170e4f1873aebb141097728b9457982ac
HanGuo97 <alex.guohan1106@outlook.com>
2018-04-13 12:37:57 -0400

    Models/PairwiseClassif: used RMS

--- TFLibrary/Models/run.py
+++ TFLibrary/Models/run.py
@@ -93,8 +93,8 @@ def build_model(data_batch,
         token_vocab_size=token_vocab_size,
         token_embedding_size=128,
         # optimization
-        optimizer="SGD",
-        learning_rate=0.5,
+        optimizer=tf.train.RMSPropOptimizer,
+        learning_rate=0.001,
         gradient_clipping_norm=2.0,
         # misc
         graph=graph,
