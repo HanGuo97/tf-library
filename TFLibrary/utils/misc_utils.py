1d144ac303c06c118f87ab3bdba450f164bbbac2
HanGuo97 <alex.guohan1106@outlook.com>
2018-05-28 00:04:34 -0400

    terminal print color support

--- TFLibrary/utils/misc_utils.py
+++ TFLibrary/utils/misc_utils.py
@@ -95,3 +95,14 @@ def align_on_references(source_references, source_outputs, target_references):
         raise ValueError("Alignment Failed")
     
     return aligned_outputs
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
