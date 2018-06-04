82c6321d1d10779922f8d2f2e97cf3811c52e47f
HanGuo97 <alex.guohan1106@outlook.com>
2018-06-03 23:12:31 -0400

    added merge_dicts to misc_utils

--- TFLibrary/utils/misc_utils.py
+++ TFLibrary/utils/misc_utils.py
@@ -8,6 +8,17 @@ from collections import deque
 from contextlib import contextmanager
 
 
+def merge_dicts(*dict_args):
+    """
+    Given any number of dicts, shallow copy and merge into a new dict,
+    precedence goes to key value pairs in latter dicts.
+    """
+    result = {}
+    for dictionary in dict_args:
+        result.update(dictionary)
+    return result
+
+
 def unique_ordered_list(seq):
     seen = set()
     seen_add = seen.add
