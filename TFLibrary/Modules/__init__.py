07b58c8ee76b672011eb51f4291b7ee400390da8
HanGuo97 <alex.guohan1106@outlook.com>
2018-06-25 15:01:07 -0400

    added Transformer to the Modules.__init__

--- TFLibrary/Modules/__init__.py
+++ TFLibrary/Modules/__init__.py
@@ -1,3 +1,4 @@
 from TFLibrary.Modules.base import AbstractModule
 from TFLibrary.Modules.embedding import Embeddding
 from TFLibrary.Modules.encoders import LstmEncoder
+from TFLibrary.Modules.transformer import TransformerEncoder
