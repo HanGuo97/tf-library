86ab86770a0189c985d62cc774a5d813465acd3d
HanGuo97 <alex.guohan1106@gmail.com>
2018-09-25 18:06:04 -0400

    Modules: added HubEmbedding to Modules.__init__

--- TFLibrary/Modules/__init__.py
+++ TFLibrary/Modules/__init__.py
@@ -1,4 +1,5 @@
 from TFLibrary.Modules.base import AbstractModule
 from TFLibrary.Modules.embedding import Embeddding
+from TFLibrary.Modules.embedding import TFHubElmoEmbedding
 from TFLibrary.Modules.encoders import LstmEncoder
 from TFLibrary.Modules.transformer import TransformerEncoder
