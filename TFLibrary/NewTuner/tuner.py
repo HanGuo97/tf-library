d6d5392fa4de5eb1a0ee6c5b0a383c1cd0e90a28
HanGuo97 <alex.guohan1106@gmail.com>
2019-02-03 22:36:01 -0500

    NewTuner: a runnable version with my own implementation

--- TFLibrary/NewTuner/tuner.py
+++ TFLibrary/NewTuner/tuner.py
@@ -1,15 +1,38 @@
 """lbbroker3: Load balancing broker using zloop in Python"""
 
 from __future__ import print_function
-import threading
-import time
-import zmq
 
+import time
+import threading
+from collections import namedtuple
+
+import zmq
 from zmq.eventloop.ioloop import IOLoop
 from zmq.eventloop.zmqstream import ZMQStream
 
+from TFLibrary.NewTuner import utils
+
 NBR_CLIENTS = 10
 NBR_WORKERS = 3
+ENCODE_METHOD = "ascii"
+
+ServerMessages = namedtuple(
+    "ServerMessages",
+    ("worker_address",
+     "client_address",
+     "reply"))
+
+ClientMessages = namedtuple(
+    "ClientMessages",
+    ("client_address", "request"))
+
+
+def encode(string):
+    return string.encode(ENCODE_METHOD)
+
+
+def decode(string):
+    return string.decode(ENCODE_METHOD)
 
 
 def worker_thread(worker_url, i):
@@ -24,7 +47,7 @@ def worker_thread(worker_url, i):
     socket.connect(worker_url)
 
     # Tell the broker we are ready for work
-    socket.send(b"READY")
+    socket.send(utils.HELLO_MESSAGE)
 
     try:
         while True:
@@ -32,9 +55,9 @@ def worker_thread(worker_url, i):
             address, empty, request = socket.recv_multipart()
 
             print("%s: %s\n" % (socket.identity.decode('ascii'),
-                                request.decode('ascii')), end='')
+                                utils.deserialize_pyobj(request)), end='')
 
-            socket.send_multipart([address, b'', b'OK'])
+            socket.send_multipart([address, utils.EMPTY, utils.serialize_pyobj('OK')])
 
     except zmq.ContextTerminated:
         # context terminated so quit silently
@@ -53,76 +76,124 @@ def client_thread(client_url, i):
     socket.connect(client_url)
 
     #  Send request, get reply
-    socket.send(b"HELLO")
+    socket.send(utils.serialize_pyobj("HELLO"))
     reply = socket.recv()
 
     print("%s: %s\n" % (socket.identity.decode('ascii'),
-                        reply.decode('ascii')), end='')
+                        utils.deserialize_pyobj(reply)), end='')
 
 
 class LRUQueue(object):
     """LRUQueue class using ZMQStream/IOLoop for event dispatching"""
 
     def __init__(self, backend_socket, frontend_socket):
-        self.available_workers = 0
-        self.workers = []
+        self._workers = []
         self.client_nbr = NBR_CLIENTS
 
-        self.backend = ZMQStream(backend_socket)
-        self.frontend = ZMQStream(frontend_socket)
-        self.backend.on_recv(self.handle_backend)
+        self._backend = ZMQStream(backend_socket)
+        self._frontend = ZMQStream(frontend_socket)
+        self._backend.on_recv(self._handle_backend)
 
-        self.loop = IOLoop.instance()
+        self._loop = IOLoop.instance()
 
-    def handle_backend(self, msg):
+    @property
+    def available_workers(self):
+        return len(self._workers)
+        """Serialize worker and Client"""
+
+    def _handle_backend(self, messages):
+        if self.available_workers >= NBR_WORKERS:
+            raise ValueError("Available Workers Wrong")
+
         # Queue worker address for LRU routing
-        worker_addr, empty, client_addr = msg[:3]
-
-        assert self.available_workers < NBR_WORKERS
-
+        (server_says_hello,
+         server_messages) = self.receive_from_servers(messages)
         # add worker back to the list of workers
-        self.available_workers += 1
-        self.workers.append(worker_addr)
-
-        #   Second frame is empty
-        assert empty == b""
+        self._workers.append(server_messages.worker_address)
 
         # Third frame is READY or else a client reply address
         # If client reply, send rest back to frontend
-        if client_addr != b"READY":
-            empty, reply = msg[3:]
-
-            # Following frame is empty
-            assert empty == b""
-
-            self.frontend.send_multipart([client_addr, b'', reply])
+        if not server_says_hello:
+            self.send_to_clients(server_messages)
 
             self.client_nbr -= 1
 
             if self.client_nbr == 0:
                 # Exit after N messages
-                self.loop.add_timeout(time.time() + 1, self.loop.stop)
+                self._loop.add_timeout(time.time() + 1, self._loop.stop)
 
         if self.available_workers == 1:
             # on first recv, start accepting frontend messages
-            self.frontend.on_recv(self.handle_frontend)
+            self._frontend.on_recv(self._handle_frontend)
 
-    def handle_frontend(self, msg):
-        # Now get next client request, route to LRU worker
-        # Client request is [address][empty][request]
-        client_addr, empty, request = msg
-
-        assert empty == b""
-
+    def _handle_frontend(self, messages):
+        # Receive and Process Messages
+        client_messages = self.receive_from_clients(messages)
         #  Dequeue and drop the next worker address
-        self.available_workers -= 1
-        worker_id = self.workers.pop()
+        worker_address = self._workers.pop()
+        # Route the messages to Servers
+        self.send_to_servers(
+            worker_address=worker_address,
+            client_messages=client_messages)
 
-        self.backend.send_multipart(
-            [worker_id, b'', client_addr, b'', request])
         if self.available_workers == 0:
             # stop receiving until workers become available again
-            self.frontend.stop_on_recv()
+            self._frontend.stop_on_recv()
+
+    def receive_from_clients(self, messages):
+        if len(messages) != 3:
+            raise ValueError("`len(messages)` should be 3")
+
+        # Now get next client request, route to LRU worker
+        # Client request is [address][empty][request]
+        client_addr, empty, request = messages
+        utils.assert_empty(empty)
+
+        return ClientMessages(
+            client_address=client_addr,
+            request=utils.deserialize_pyobj(request))
+
+
+    def receive_from_servers(self, messages):
+        if len(messages) not in [3, 5]:
+            raise ValueError("`len(messages)` should be in [3, 5]")
+
+        # Queue worker address for LRU routing
+        worker_address, empty, client_address = messages[:3]
+        utils.assert_empty(empty)
+
+        # Third frame is READY or else a client reply address
+        # If client reply, send rest back to frontend
+        server_says_hello = utils.is_hello_message(client_address)
+        if not server_says_hello:
+            empty, reply = messages[3:]
+            utils.assert_empty(empty)
+        else:
+            reply = None
+
+        return server_says_hello, ServerMessages(
+            worker_address=worker_address,
+            client_address=client_address,
+            reply=utils.deserialize_pyobj(reply) if reply else None)
+
+
+    def send_to_clients(self, server_messages):
+        if not isinstance(server_messages, ServerMessages):
+            raise TypeError
+
+        self._frontend.send_multipart([
+            server_messages.client_address, utils.EMPTY,
+            utils.serialize_pyobj(server_messages.reply)])
+
+
+    def send_to_servers(self, worker_address, client_messages):
+        if not isinstance(client_messages, ClientMessages):
+            raise TypeError
+
+        self._backend.send_multipart([
+            worker_address, utils.EMPTY,
+            client_messages.client_address, utils.EMPTY,
+            utils.serialize_pyobj(client_messages.request)])
 
 
 def main():
@@ -155,3 +226,7 @@ def main():
 
     # start reactor
     IOLoop.instance().start()
+
+
+if __name__ == "__main__":
+    main()
---
+++ TFLibrary/NewTuner/utils.py
@@ -0,0 +1,33 @@
+try:
+    import cPickle
+    pickle = cPickle
+except ImportError:
+    cPickle = None
+    import pickle
+
+try:
+    DEFAULT_PROTOCOL = pickle.DEFAULT_PROTOCOL
+except AttributeError:
+    DEFAULT_PROTOCOL = pickle.HIGHEST_PROTOCOL
+
+
+# Empty Deliminator is used by the socket lib, so must be bytes
+EMPTY = b""
+HELLO_MESSAGE = b"READY"
+
+
+def assert_empty(empty):
+    if empty != EMPTY:
+        raise ValueError("Empty should be empty")
+
+
+def is_hello_message(message):
+    return message == HELLO_MESSAGE
+
+
+def serialize_pyobj(obj, protocol=DEFAULT_PROTOCOL):
+    return pickle.dumps(obj, protocol)
+
+
+def deserialize_pyobj(messages):
+    return pickle.loads(messages)
